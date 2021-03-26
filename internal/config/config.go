ca5f8096d1bf272f31c479f0af3d4886f6b07484
mohammadkarimi23 <eecs.mk74@gmail.com>
2021-03-26 19:43:11 +0100

    use env variables if config file is not available

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,35 +6,54 @@ import (
 	"log"
 )
 
+type Config struct {
+	APIServerAddress string `mapstructure:"API_SERVER_ADDRESS"`
+	TelegramAPIToken string `mapstructure:"TELEGRAM_API_TOKEN"`
+}
+
 var (
-	ConfigPath string
+	ConfigName string
+	AppConfig  Config
 )
 
 func init() {
 	// Register flags
-	flag.StringVar(&ConfigPath, "config-path", "", "Path to config directory")
+	flag.StringVar(&ConfigName, "config-name", "", "Path to config directory")
 	flag.Parse()
 
-	initMamalConfig()
+	loadConfig(&AppConfig)
 }
 
 func loadConfig(configStruct interface{}) {
 	viperInstance := viper.New()
 
-	viperInstance.SetConfigName(ConfigPath)
-	viperInstance.AddConfigPath(".")
-	viperInstance.SetConfigType("env")
+	// If Configname is passed, read the config from file
+	if ConfigName != "" {
+		viperInstance.SetConfigName(ConfigName)
+		viperInstance.AddConfigPath(".")
+		viperInstance.SetConfigType("env")
 
-	viper.AutomaticEnv()
+		err := viperInstance.ReadInConfig()
+		if err != nil {
+			log.Panic("Config file is not set", err)
+		}
 
-	err := viperInstance.ReadInConfig()
-	if err != nil {
-		log.Panic("failed to read Config, ", err)
+		err = viperInstance.Unmarshal(configStruct)
+		if err != nil {
+			log.Panic("Failed to Unmarshal Config file: ", err)
+		}
+		log.Printf("loaded config: %v\n", ConfigName)
+	} else {
+		viperInstance.AutomaticEnv()
+		serverAddr := viperInstance.GetString("API_SERVER_ADDRESS")
+		apiToken := viperInstance.GetString("TELEGRAM_API_TOKEN")
+		if len(serverAddr) > 0 && len(apiToken) > 0 {
+			AppConfig = Config{
+				APIServerAddress: serverAddr,
+				TelegramAPIToken: apiToken,
+			}
+		} else {
+			log.Panic("ENV Vars should be set")
+		}
 	}
-
-	err = viperInstance.Unmarshal(configStruct)
-	if err != nil {
-		log.Panic("failed to unmarshal Config file")
-	}
-	log.Printf("loaded config: %v\n", ConfigPath)
 }
--- internal/telegram/markup.go
+++ internal/telegram/markup.go
@@ -8,7 +8,7 @@ import (
 
 func getAuthMessage(userID string) tgbotapi.InlineKeyboardMarkup {
 
-	link := config.AppConfig.Webserver.Address + "/auth/telegram?user_id=" + userID //FIXME dev config
+	link := config.AppConfig.APIServerAddress + "/auth/telegram?user_id=" + userID //FIXME dev config
 	return tgbotapi.NewInlineKeyboardMarkup(
 		tgbotapi.NewInlineKeyboardRow(
 			tgbotapi.NewInlineKeyboardButtonURL("Login to Spotify", link),
